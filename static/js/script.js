e50939f5abca8ca1a911aec4ae6d85476280276d
sarah2244-4 <douthwaite.sarah@yahoo.co.uk>
2024-03-20 11:53:59 +0000

    Added navbar with js to select active page

---
+++ static/js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+	setActiveLink();
+});
+
+function setActiveLink() {
+	var path = location.pathname;
+	var pageName;
+
+	if (path === "/") {
+		pageName = "home";
+	} else if (path.startsWith("/printables")) {
+		pageName = "printables";
+	}
+
+	var liElement = document.querySelector(
+		"li[data-link-name='" + pageName + "']"
+	);
+	if (liElement) {
+		liElement.classList.add("active");
+	}
+}
