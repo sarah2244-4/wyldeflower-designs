879d083f54e730f302dd4c2f7b413d964a22cd27
sarah2244-4 <douthwaite.sarah@yahoo.co.uk>
2024-04-03 12:13:42 +0100

    Added form to home.html

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import NewsletterSignupForm
 
 
 def index(request):
@@ -6,3 +7,15 @@ def index(request):
     View the index page
     """
     return render(request, 'home/index.html')
+
+
+def newsletter_signup_view(request):
+    if request.method == 'POST':
+        form = NewsletterSignupForm(request.POST)
+        if form.is_valid():
+            # Process the form data
+            form.save(request)
+            return redirect('home')
+    else:
+        form = NewsletterSignupForm()
+    return render(request, 'signup.html', {'form': form})
--- wyldeflower_kids/urls.py
+++ wyldeflower_kids/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from home.views import newsletter_signup_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('newsletter/signup/', newsletter_signup_view, name='newsletter_signup'),
 ] + static(settings.MEDIA_URL,
 document_root=settings.MEDIA_ROOT)
