7185cf38ed0dd9b5cd5d742d7101c200e63bd93d
sarah2244-4 <douthwaite.sarah@yahoo.co.uk>
2024-04-05 14:02:52 +0100

    Added profile views and form

--- users/views.py
+++ users/views.py
@@ -3,8 +3,26 @@ from django.contrib.auth.decorators import login_required
 
 
 @login_required
-def view_profile(request):
-    """
-    View the profile page
-    """
-    return render(request, 'profile.html')
+def profile(request):
+    """ Display the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
+    template = 'profiles/profile.html'
+    context = {
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
+    }
+
+    return render(request, template, context)
