6b06dc49e48d37b8ab52e54d7e15ae9662a1ad8a
sarah2244-4 <douthwaite.sarah@yahoo.co.uk>
2024-04-05 13:16:31 +0100

    Added views to view and sort items

--- items/models.py
+++ items/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
 
-class Type(models.Model):
-    name = models.CharField(max_length=50)
-
-
 class Category(models.Model):
 
     class Meta:
@@ -24,7 +20,6 @@ class Item(models.Model):
     size = models.CharField(max_length=25, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    type = models.ForeignKey('Type', null=True, blank=True, on_delete=models.SET_NULL)
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
--- items/views.py
+++ items/views.py
@@ -1,3 +1,81 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.db.models.functions import Lower
 
-# Create your views here.
+from .models import Category, Item
+from .forms import ItemForm
+
+
+def all_items(request):
+    """ 
+    A view to show all items, including sorting and search queries 
+    """
+
+    items = Item.objects.all()
+    query = None
+    categories = None
+    types = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                items = items.annotate(lower_name=Lower('name'))
+
+            if sortkey == 'type':
+                sortkey = 'type__name'
+
+            if sortkey == 'category':
+                sortkey = 'category__name'
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            items = items.order_by(sortkey)
+            
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            items = items.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('items'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            items = items.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
+
+    context = {
+        'items': items,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
+    }
+
+    return render(request, 'items/items.html', context)
+
+
+def item_detail(request, item_id):
+    """
+    A view to show individual item details
+    """
+
+    item = get_object_or_404(Item, pk=item_id)
+
+    context = {
+        'item': item,
+    }
+
+    return render(request, 'items/item_detail.html', context)
