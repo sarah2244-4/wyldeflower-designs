9a33e813a3f81830c2824a10483e59b650f45da4
sarah2244-4 <douthwaite.sarah@yahoo.co.uk>
2024-04-05 13:16:55 +0100

    Added views to add, edit or delete items

--- items/views.py
+++ items/views.py
@@ -79,3 +79,78 @@ def item_detail(request, item_id):
     }
 
     return render(request, 'items/item_detail.html', context)
+
+
+
+@login_required
+def add_item(request):
+    """ 
+    A view for an admin to add an item to the store
+    """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            item = form.save()
+            messages.success(request, 'Successfully added item!')
+            return redirect(reverse('item_detail', args=[item.id]))
+        else:
+            messages.error(request, 'Failed to add item. Please ensure the form is valid.')
+    else:
+        form = ItemForm()
+        
+    template = 'items/add_item.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_item(request, item_id):
+    """
+    A view for an admin to edit an item in the store
+    """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    item = get_object_or_404(Item, pk=item_id)
+    if request.method == 'POST':
+        form = ItemForm(request.POST, request.FILES, instance=item)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated!')
+            return redirect(reverse('item_detail', args=[item.id]))
+        else:
+            messages.error(request, 'Failed to update item. Please ensure the form is valid.')
+    else:
+        form = ItemForm(instance=item)
+        messages.info(request, f'You are editing {item.name}')
+
+    template = 'items/edit_item.html'
+    context = {
+        'form': form,
+        'item': item,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_item(request, item_id):
+    """
+    A view for an admin to delete an item completely from the store
+    """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    item = get_object_or_404(Item, pk=item_id)
+    item.delete()
+    messages.success(request, 'Item deleted!')
+    return redirect(reverse('items'))
