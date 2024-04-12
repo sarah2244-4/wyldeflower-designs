4dac46119f9b704434ca7192c6a31dcf9c0f6fa6
sarah2244-4 <douthwaite.sarah@yahoo.co.uk>
2024-04-12 12:45:18 +0100

    Added bag urls

--- wyldeflower_kids/urls.py
+++ wyldeflower_kids/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('profile/', include('profiles.urls')),
     path('products/', include('products.urls')),
+    path('bag', include('bag.urls')),
 ] + static(settings.MEDIA_URL,
 document_root=settings.MEDIA_ROOT)
