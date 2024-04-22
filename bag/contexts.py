c0f5e7e3e6295faeb810aca6e94db9bd5f2ff5ca
sarah2244-4 <douthwaite.sarah@yahoo.co.uk>
2024-04-22 17:41:46 +0100

    Removed quantity spin buttons and added subtotal to views

--- bag/contexts.py
+++ bag/contexts.py
@@ -6,23 +6,26 @@ from products.models import Product
 def bag_contents(request):
 
     bag_items = []
-    total = 0
+    total = Decimal('0')
     product_count = 0
+    subtotal = 0
+    delivery_percentage = Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
     bag = request.session.get('bag', {})
 
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
+        subtotal += quantity * product.price
+        total += subtotal
         product_count += quantity
         bag_items.append({
             'item_id': item_id,
             'quantity': quantity,
             'product': product,
+            'subtotal': subtotal,
         })
 
-    delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-    
-    grand_total = delivery + total
+    delivery = total * (delivery_percentage / Decimal('100'))
+    grand_total = total + delivery
     
     context = {
         'bag_items': bag_items,
@@ -30,6 +33,7 @@ def bag_contents(request):
         'product_count': product_count,
         'delivery': delivery,
         'grand_total': grand_total,
+        'subtotal': subtotal,
     }
 
     return context
