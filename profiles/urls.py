4a20d7b7a78a93f8ca51ad8f9b8c4133a951f6f0
sarah2244-4 <douthwaite.sarah@yahoo.co.uk>
2024-04-30 14:22:38 +0100

    Added order history onto profile

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.profile, name='profile'),
     path('edit/<int:user_id>/', views.edit_profile, name='edit_profile'),
     path('add/<int:product_id>/<slug>/', views.add_to_wishlist, name='add_to_wishlist'),
+    path('order_history/<order_number>', views.order_history, name='order_history'),
 ]
