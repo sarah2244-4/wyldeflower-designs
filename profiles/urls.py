1599a28c212bfb12d0c2dc1e48d226aa237d0dcf
sarah2244-4 <douthwaite.sarah@yahoo.co.uk>
2024-04-30 13:51:27 +0100

    Added add_to_wishlist view

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
+    path('add/<int:product_id>/<slug>/', views.add_to_wishlist, name='add_to_wishlist'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+
 from .models import UserProfile
 from .forms import UserProfileForm
+
 from checkout.models import Order
+from profiles.models import Wishlist
+from products.models import Product
 
 
 @login_required
@@ -47,3 +51,22 @@ def order_history(request, order_number):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def add_to_wishlist(request, product_id, slug):
+    product = get_object_or_404(Product, id=product_id, slug=slug)
+
+    if request.method == 'POST':
+        wished_product, created = Wishlist.objects.get_or_create(
+            wished_product=product,
+            user=request.user,
+        )
+        if created:
+            messages.success(request, 'The item was added to your wishlist')
+        else:
+            messages.info(request, 'The item is already in your wishlist')
+    else:
+        messages.error(request, 'Update failed. Please ensure the form is valid.')
+
+    return redirect(reverse('product_detail', args=[product_id, slug]))
